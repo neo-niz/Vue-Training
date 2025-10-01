4efee5e6f0ea011f34aa368d96fcb6210131f3c2
Nizar Belkacem <nizar.belkacem@NX-FR-LT757.local>
2025-10-01 11:59:13 +0200

    feat(bloc1): Simple Hello vue app

--- vue-neo-training1/src/App.vue
+++ vue-neo-training1/src/App.vue
@@ -1,10 +1,22 @@
-<script setup></script>
+<!-- <script setup>
+  import { ref } from 'vue';
+  const message = ref("Hello vue!")
+  
+</script> -->
+
+<script>
+export default{
+  data(){
+    return {
+      message: "Hello vue!"
+    }
+  }
+}
+</script>
 
 <template>
-  <h1>You did it!</h1>
   <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
+    {{ message }}
   </p>
 </template>
 
