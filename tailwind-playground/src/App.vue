76c32835319aa33f44504723b6e872b8a1c48ba4
Nizar Belkacem <nizar.belkacem@NX-FR-LT757.local>
2025-10-03 15:18:41 +0200

    feat(bloc1): Article page

--- tailwind-playground/src/App.vue
+++ tailwind-playground/src/App.vue
@@ -2,10 +2,21 @@
 </script>
 
 <template>
-  <main >
-    <h1 class="text-red-500 text-2xl">
-      I am using tailwind
-    </h1>
+  <main class="space-x-24 min-h-screen flex items-center justify-center bg-gray-50 p-8 m-12" >
+    <article class="bg-white shadow-lg rounded-3xl p-6">
+      <h1 class="text-indigo-800 text-4xl font-bold tracking-wide">
+        I am using tailwind
+      </h1>
+      <p class=" font-medium text-gray-800">
+         This is a simple article page built with Vue 3 and Tailwind CSS. 
+        The heading, paragraph, and button each use different font sizes, 
+        weights, and colors
+      </p>
+      <button class=" bg-rose-400 hover:bg-green-950 rounded-xl shadow-lg font-serif transition">
+        Click me 
+      </button>
+
+    </article>
   </main>
 </template>
 
