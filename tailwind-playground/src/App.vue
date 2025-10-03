c55ad6e042c11444ae5ba8992479d7804e0a1718
Nizar Belkacem <nizar.belkacem@NX-FR-LT757.local>
2025-10-03 17:01:52 +0200

    feat(bloc4): Responseive nav bar

--- tailwind-playground/src/App.vue
+++ tailwind-playground/src/App.vue
@@ -5,10 +5,32 @@ import BlocTwoCard from './components/blocs/blocTwoCard.vue';
 
 <template>
   <div class="min-h-screen flex flex-col bg-gray-100">
-    <header class="flex bg-rose-400 justify-between p-3 items-center shadow-lg ">
-      <h1 class="text-3xl text-white font-bold">
-        Tailwind playground
-      </h1>
+    <header class=" bg-rose-400 shadow">
+      <div class="hidden md:flex p-4 container items-center">
+        <h1 class="text-2xl text-white font-bold ">
+          Tailwind playground
+        </h1>
+        <nav class="pl-8 flex gap-4">
+          <a href="#" class="text-white text-xl font-bold hover:underline">Home</a>
+          <a href="#" class="text-white text-xl font-bold hover:underline">About</a>
+          <a href="#" class="text-white text-xl font-bold hover:underline">Services</a>
+          <a href="#" class="text-white text-xl font-bold hover:underline">Contact</a>
+        </nav>
+        
+      </div>
+
+      <div class="flex md:hidden p-2 flex-col items-center">
+        <h1 class="text-2xl text-white font-bold ">
+          Tailwind playground
+        </h1>
+        <nav class="pt-4 flex flex-col gap-2 items-center">
+          <a href="#" class="text-white text-xl font-bold hover:underline">Home</a>
+          <a href="#" class="text-white text-xl font-bold hover:underline">About</a>
+          <a href="#" class="text-white text-xl font-bold hover:underline">Services</a>
+          <a href="#" class="text-white text-xl font-bold hover:underline">Contact</a>
+        </nav>
+        
+      </div>
     </header>
 
     <main class="flex-1 p-6">
