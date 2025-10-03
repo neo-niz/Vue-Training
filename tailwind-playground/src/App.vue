ba409d0033d64fbd138188d72fa3d050ecd9dc27
Nizar Belkacem <nizar.belkacem@NX-FR-LT757.local>
2025-10-03 14:40:29 +0200

    feat(tailwind): init project

---
+++ tailwind-playground/src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+</script>
+
+<template>
+  <main >
+    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+      Click Me
+    </button>
+  </main>
+</template>
+
+<style scoped>
+</style>
