05378715c6ec80766a033cbec5f81b469b73d275
Nizar Belkacem <nizar.belkacem@NX-FR-LT757.local>
2025-10-03 14:46:43 +0200

    feat(bloc0): Configure & test tailwind

--- tailwind-playground/src/App.vue
+++ tailwind-playground/src/App.vue
@@ -3,9 +3,9 @@
 
 <template>
   <main >
-    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-      Click Me
-    </button>
+    <h1 class="text-red-500 text-2xl">
+      I am using tailwind
+    </h1>
   </main>
 </template>
 
